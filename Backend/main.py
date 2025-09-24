6fc09abf6c2cf34081b0fbf5bb93e1c1bc5fba09
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-09-24 12:02:35 +0545

    Sending file via http

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File 
 from fastapi.middleware.cors import CORSMiddleware
+from audio_decode import decode_fsk
+import numpy as np
+import io
+from scipy.io import wavfile
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/decode")
+async def decode_audio(file: UploadFile = File(...)):
+    contents = await file.read()
+    wav_bytes = io.BytesIO(contents)
+    fs, signal = wavfile.read(wav_bytes)
+
+    if signal.ndim > 1:
+        signal = signal[:,0]
+
+    bitstream, message = decode_fsk(signal, fs = fs)
+    return {"bitstream": bitstream, "message": message}
 
