b343f498eb23d535065d7ed012fd90b0d368d5a4
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-09-26 14:11:08 +0545

    audio sqlite

--- Backend/data_base.py
+++ Backend/data_base.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from fastapi import Depends, HTTPException, Query
 from typing import Annotated
-from sqlmodel import Field, SQLModel, Session, create_engine, select
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./bitbeat.db"
 engine = create_engine(
--- Backend/models.py
+++ Backend/models.py
@@ -6,3 +6,11 @@ class User(Base, SQLModel):
     __tablename__ = "users"
     id: int = Field(default=None, primary_key=True, idex=True) # type: ignore
     username: str = Field(index=True, unique=True)
+
+class decoded_audio(Base, SQLModel):
+    __tablename__ = "decoded_audios"
+    id: int = Field(default=None, primary_key=True, idex=True) # type: ignore
+    user_id: int = Field(foreign_key="users.id")
+    bitstream: str
+    message: str
+    timestamp: str
