f599a18c0d177e519dcb51637800e37e2b89bc89
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-09-26 15:01:15 +0545

    registration page done

--- Backend/models.py
+++ Backend/models.py
@@ -1,16 +1,18 @@
 from sqlalchemy import column, Integer, String, Boolean
 from sqlmodel import Field, SQLModel
 from data_base import Base
+from datetime import datetime
 
 class User(Base, SQLModel):
     __tablename__ = "users"
-    id: int = Field(default=None, primary_key=True, idex=True) # type: ignore
+    id: int = Field(default=None, primary_key=True, idex=True)
     username: str = Field(index=True, unique=True)
 
 class decoded_audio(Base, SQLModel):
     __tablename__ = "decoded_audios"
-    id: int = Field(default=None, primary_key=True, idex=True) # type: ignore
+    id: int = Field(default=None, primary_key=True, idex=True) 
     user_id: int = Field(foreign_key="users.id")
     bitstream: str
     message: str
-    timestamp: str
+    timestamp: datetime = Field(default=datetime.utcnow) 
+
